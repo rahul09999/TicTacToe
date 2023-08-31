0ddbad9ab07090833ca29e96e8c6bb22857163e6
Rahul_lalwani <92937491+rahul09999@users.noreply.github.com>
2023-08-31 19:58:48 +0530

    Update createBoard.py
    Solved error by removing "-" from line 5

--- lib/createBoard.py
+++ lib/createBoard.py
@@ -2,5 +2,5 @@ def createBoard(board, boardSize):
     for i in range(0, boardSize):
         board.append([])
         for j in range(0, boardSize):
-            board[i].append('-')
+            board[i].append(' ')
     # return board
